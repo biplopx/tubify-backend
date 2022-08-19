81b35c29830ace3a7033e1dc6ecd894b4d6fd7f4
mahedi <mahedianik4@gmail.com>
2022-08-19 16:28:15 +0600

    text playlist api

--- routes/playlistsRouter.js
+++ routes/playlistsRouter.js
@@ -4,41 +4,40 @@ const User = require('../models/usersModel');
 const songModel = require('../models/songModel');
 const playlistsRouter = express.Router();
 
-playlistsRouter.put('/:id', async (req, res) => {
+playlistsRouter.post('/:id', async (req, res) => {
     const songId = req.params.id;
     const userId = req.body.userId;
-    const customPlayListName = "happy";
+    const customPlayListName = "sad";
     try {
         const user = await User.findById(userId);
         const customPlayList = {
             name: customPlayListName,
-            songs: [songId,]
+            songs: [songId]
         }
         const previousPlayList = user.playlist;
         const previousPlayListName = previousPlayList.map(playlist => playlist.name);
 
         const previousPlayListSongs = previousPlayList.map(playlist => playlist.songs);
 
-        const n = previousPlayListSongs.flat()
+        const flatSongs = previousPlayListSongs.flat()
+
         // console.log(n)
         if (previousPlayListName.includes(customPlayListName)) {
-            if (n.includes(songId)) {
-                res.send({ message: "song already exist" })
+            if (flatSongs.includes(songId)) {
+                return res.send({ message: "song already exist" })
+            } else {
+                const song = user.playlist.find(playlist => playlist.name === customPlayListName)
+                song.songs.push(songId)
+                console.log(song.songs)
+                user.save(song);
+                res.status(200).json({ status: "add successful" });
             }
-           
-                previousPlayList.map( async playlist => {
-                    const [...songs] = playlist.songs;
-                    songs.push(songId);
-                    console.log(playlist)
-                    await user.save();
-                   return res.status(200).json({ status: "add successful" });
-                });
-           
+
         }
         else {
             const newPlaylist = user.playlist.push(customPlayList);
             const result = await user.save(newPlaylist);
-            res.status(200).json({ status: "successful" });
+            return res.status(200).json({ status: "successful" });
         };
     } catch (err) {
         res.send(err)
